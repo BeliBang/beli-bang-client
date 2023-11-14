5a61f3f9d9efd78a16046e60564476cd72096737
dkytmshrn <dkytmshrn@gmail.com>
2023-11-14 23:43:56 +0700

    feat: register-screen

---
+++ BeliBang-client/assets/styles/styles-lib.js
@@ -0,0 +1,97 @@
+import { StyleSheet } from "react-native"
+
+const stylesLib = StyleSheet.create({
+    fontFam : {
+        fontFamily: 'sans-serif'
+    },
+    btnSm : {
+        width: 100,
+        
+    },
+    flex1 : {
+        flex: 1
+    },
+    flex2 : {
+        flex: 2
+    },
+    flex3 : {
+        flex: 3
+    },
+    flex4 : {
+        flex: 4
+    },
+    flex5 : {
+        flex: 5
+    },
+    flex6 : {
+        flex: 6
+    },
+    flex7 : {
+        flex: 7
+    },
+    flex8 : {
+        flex: 8
+    },
+    flex9 : {
+        flex: 9
+    },
+    w1 : {
+        width: 50
+    },
+    w2 : {
+        width: 100
+    },
+    w3 : {
+        width: 150
+    },
+    w4 : {
+        width: 200
+    },
+    w5 : {
+        width: 250
+    },
+    w6 : {
+        width: 300
+    },
+    w7 : {
+        width: 350
+    },
+    w8 : {
+        width: 400
+    },
+    w9 : {
+        width: 450
+    },
+    colGrBold: {
+        color: 'rgb(58, 77, 57)'
+    },
+    colGr: {
+        color: 'rgb(79, 111, 82)'
+    },
+    colGrLight: {
+        color: 'rgb(115, 144, 114)'
+    },
+    colCrBold: {
+        color: 'rgb(0, 255, 120)'
+    },
+    colCr: {
+        color: 'rgb(236, 227, 206)'
+    },
+    bgColGrBold: {
+        backgroundColor: 'rgb(58, 77, 57)'
+    },
+    bgColGr: {
+        backgroundColor: 'rgb(79, 111, 82)'
+    },
+    bgColGrLight: {
+        backgroundColor: 'rgb(115, 144, 114)'
+    },
+    bgColCrBold: {
+        backgroundColor: 'rgb(0, 255, 120)'
+    },
+    bgColCr: {
+        backgroundColor: 'rgb(236, 227, 206)'
+    }
+  })
+
+export default stylesLib
--- BeliBang-client/src/screens/RegisterScreen.js
+++ BeliBang-client/src/screens/RegisterScreen.js
@@ -1,9 +1,36 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import stylesLib from '../../assets/styles/styles-lib';
 
 export default function RegisterScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Register Screen</Text>
+    <View style={[stylesLib.bgColGr, stylesLib.flex1]}>
+      <View>
+        <Text style={[stylesLib.colCr]}>username</Text>
+        <TextInput style={[stylesLib.bgColCr]} />
+      </View>
+      <View>
+        <Text style={[stylesLib.colCr]}>password</Text>
+        <TextInput style={[stylesLib.bgColCr]} />
+      </View>
+      <View>
+        <Text style={[stylesLib.colCr]}>Already have an account?</Text>
+        <Text style={[stylesLib.colCr]}>log in here</Text>
+      </View>
+      <View>
+        <TouchableOpacity style={[stylesLib.bgColCr]}>
+          <Text>Buyer</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[stylesLib.bgColCr]}>
+          <Text>Seller</Text>
+        </TouchableOpacity>
+        </View>
+      <View>
+        <TouchableOpacity style={[stylesLib.bgColCr, stylesLib.w9]}>
+          <Text>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+
